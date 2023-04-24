76c5c5f557be372b1478cc43e65f343dc8e4edb0
Eduard Belan <eduard.belan.git@gmail.com>
2023-04-24 19:57:31 +0200

    Make 'Name Directories' section bulletproof. Set DONE in bugs.txt

--- dir-maker.py
+++ dir-maker.py
@@ -18,13 +18,23 @@ while not is_int:
         print("only num input valid")
 
 # Loop to make dirs
+forbidden_symbols = ['/', '\\', ':', '*', '?', '"', '<', '>', '|', ' ']
 fruit_loops = 0
 for i in range(num_dirs):
     fruit_loops += 1
     # Ask User to name dirs
-    name_dirs = input(f"name dir {fruit_loops}: ")
+    dir_name = input(f"name dir {fruit_loops}: ")
+    # Check for forbidden symbols
+    while any(symbol in dir_name for symbol in forbidden_symbols):
+        dir_name = input(f"don't use: {forbidden_symbols}\nnew name dir {fruit_loops}: ")
+    # Check if dir name already exists
+    while os.path.exists(f"C:\\Users\\eduar\\portfolio-projects\\{dir_name}"):
+        dir_name = input(f"'{dir_name}' already exists.\nnew name dir {fruit_loops}: ")
+        # Check for forbidden symbols
+        while any(symbol in dir_name for symbol in forbidden_symbols):
+            dir_name = input(f"don't use: {forbidden_symbols}\nnew name dir {fruit_loops}: ")
     # Make dir
-    os.mkdir(f"C:\\Users\\eduar\\portfolio-projects\\{name_dirs}")
+    os.mkdir(f"C:\\Users\\eduar\\portfolio-projects\\{dir_name}")
     # Ask User if files
     file_dirs = input("need files y/n: ").lower()
     if file_dirs == "n":
@@ -36,11 +46,11 @@ for i in range(num_dirs):
         file_name = input("name file: ")
         if what_file == "txt":
             # Make file
-            file_path = f"C:\\Users\\eduar\\portfolio-projects\\{name_dirs}\\{file_name}.txt"
+            file_path = f"C:\\Users\\eduar\\portfolio-projects\\{dir_name}\\{file_name}.txt"
             open(file_path, "a").close()
         else:
             # Make file
-            file_path = f"C:\\Users\\eduar\\portfolio-projects\\{name_dirs}\\{file_name}.py"
+            file_path = f"C:\\Users\\eduar\\portfolio-projects\\{dir_name}\\{file_name}.py"
             open(file_path, "a").close()
 
 
