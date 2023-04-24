4c329bff993e94010eb62266d742e520c09d54a1
Eduard Belan <eduard.belan.git@gmail.com>
2023-04-25 00:08:26 +0200

    Section 'Ask for files': 	- check user input for y/n Section 'File Type' 	- add more file types 	- check user input for valid file type

--- dir-maker.py
+++ dir-maker.py
@@ -19,6 +19,7 @@ while not is_int:
 
 # Loop to make dirs
 forbidden_symbols = ['/', '\\', ':', '*', '?', '"', '<', '>', '|', ' ']
+file_types = ["txt", "py", "html", "css", "js"]
 fruit_loops = 0
 for i in range(num_dirs):
     fruit_loops += 1
@@ -37,22 +38,40 @@ for i in range(num_dirs):
     os.mkdir(f"C:\\Users\\eduar\\portfolio-projects\\{dir_name}")
     # Ask User if files
     file_dirs = input("need files y/n: ").lower()
+    # Check user input for y/n
+    while file_dirs not in ["y", "n"]:
+        file_dirs = input("invalid input, enter y/n for file: ")
+
     if file_dirs == "n":
         pass
     else:
         # Ask User file type
-        what_file = input("type file txt/py: ").lower()
+        what_file = input("type file txt/py/html/css/js: ").lower()
+        # Check user input for file type
+        while what_file not in ["txt", "py", "html", "css", "js"]:
+            what_file = input(f"invalid input, choose a file type from {file_types}\nenter file type: ")
         # Ask User file name
         file_name = input("name file: ")
         if what_file == "txt":
-            # Make file
+            # Make file txt
             file_path = f"C:\\Users\\eduar\\portfolio-projects\\{dir_name}\\{file_name}.txt"
             open(file_path, "a").close()
-        else:
-            # Make file
+        elif what_file == "py":
+            # Make file py
             file_path = f"C:\\Users\\eduar\\portfolio-projects\\{dir_name}\\{file_name}.py"
             open(file_path, "a").close()
-
+        elif what_file == "html":
+            # Make file html
+            file_path = f"C:\\Users\\eduar\\portfolio-projects\\{dir_name}\\{file_name}.html"
+            open(file_path, "a").close()
+        elif what_file == "css":
+            # Make file css
+            file_path = f"C:\\Users\\eduar\\portfolio-projects\\{dir_name}\\{file_name}.css"
+            open(file_path, "a").close()
+        elif what_file == "js":
+            # Make file js
+            file_path = f"C:\\Users\\eduar\\portfolio-projects\\{dir_name}\\{file_name}.js"
+            open(file_path, "a").close()
 
 # Snippets
 # make directory
